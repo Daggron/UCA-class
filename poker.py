a7e1af73e7e6844be78111a8755d796adff19641
Abhay <returnofking04@gmail.com>
2019-07-13 00:55:08 +0530

    Poker Game

--- poker.py
+++ poker.py
@@ -1,14 +1,8 @@
 
 def straight(ranks):
-    if len(set(ranks))==5 and (max(ranks)-min(ranks)==4):
+    if len(set(ranks))==5 and (max(ranks)-min(ranks) == 4):
         return True
     return False
-
-
-ranks = [4, 5, 6, 7, 8]
-suits = ['H', 'H', 'H', 'H', 'H']
-
-k = ['1', '2', '2', '8', '8']
 
 
 def flush(suits):
@@ -17,16 +11,16 @@ def flush(suits):
     return False
 
 
-def kind(n, rank):
-    for r in rank:
-        if rank.count(r) == n:
+def kind(n, ranks):
+    for r in ranks:
+        if ranks.count(r) == n:
             return r
     return None
 
 
-def two_pair(el):
-    hicard = kind(2, el)
-    locard = kind(2, list(reversed(el)))
+def two_pair(ranks):
+    hicard = kind(2, ranks)
+    locard = kind(2, list(reversed(ranks)))
 
     if hicard != locard:
         return (hicard, locard)
@@ -35,18 +29,32 @@ def two_pair(el):
 
 def card_ranks(hand):
    card_value = ['--23456789TJQK'.index(r) for r, s in hand]
-   ranks.sort(reverse = True)
+   card_value.sort(reverse = True)
    return card_value
 
 
 def card_suits(hand):
     return [s for r, s in hand]
 
+#
+# print(straight(ranks))
+# print(flush(suits))
+# print(kind(3, k))
+# print(two_pair(k))
 
-print(straight(ranks))
-print(flush(suits))
-print(kind(3, k))
-print(two_pair(k))
+
+def poker(hands):
+    return max(hands, key=hand_rank)
+
+
+def hand_rank(hand):
+    ranks = card_ranks(hand)
+    suits = card_suits(hand)
+
+    if straight(ranks) and flush(suits):
+        return (8, max(ranks))
+    elif kind(4,ranks):
+        return (7, kind(4,ranks), kind(1, ranks))
 
 
 if __name__ == '__main__':
